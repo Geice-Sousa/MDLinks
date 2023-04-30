f46e84d448e86a32dd6fc7644fd1ebb107517554
Geice Sousa Pinho <geicesousa@outlook.com>
2023-04-30 09:51:34 -0300

    como usar o chalk e o boxen, mdlinks promise

--- cli.js
+++ cli.js
@@ -1,4 +1,28 @@
-// usar .js na hora de chamar o arquivo
 const index = require('./index.js');
-console.log(index);
+const chalk = require('chalk');
+const boxen = require('boxen');
+const mdLinks = index.mdLinks('./files-test/fileMarkdown.md')[0]
+
+// console.log(mdLinks[0])
+// console.log(mdLinks[3])
+
+// a função não tá lendo os outros arqvs txt e js da erro no map pq não tem a regex pra fazer o match, esses arquivos serão tratados no catch
+//  mdLinks.then(()=>{
+  // mdLinks.map((link)=>{
+  //   console.log(link);
+  //   console.log(chalk.blue("link"));  
+  //   console.log(chalk.blue(link));  
+  //  })
+
+  //  mdLinks.forEach(element => {
+  //   console.log(element)
+  //  });
+// })
+// .catch((error)=>{
+//   // error.map((erro)=>{
+//     console.log(error);
+//   // })
+//   console.log(boxen(chalk.red('erro :'), {borderColor: 'redBright', borderStyle: 'double'})); // assim que vou usar para o erro
+// })
+
 
--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 // aqui vai ficar a função mdLinks(path, options) , não precisa chamar function, funciona como um objeto
 
-// module.exports = { // vou exportar pra usar onde??
-
-const getInfos = (string, archive)=>{
+function getInfos(string, archive){
   const contentInfos = string.split('](');
   const text = contentInfos[0].replace('[', '');
   const href = contentInfos[1].replace(')', '');
-  console.log({ href, text, archive })
+  console.log({ href, text, archive }) // se dou return não aparece as infos
 };
 
-function mdLinks(pathFile){
+module.exports.mdLinks = (pathFile)=>{
   const fs = require('fs');
   const regEx = /\[[^\]]+\]\(([^)]+)\)/gm; // gm para pegar varias linhas
-
-  fs.readFile(pathFile, 'utf-8', (error, data)=>{
-    if(error){
-      console.log('error')
-    } else {
-      const infos = data.match(regEx); // array com todos os links
-      // console.log(infos)
-      const objInfo = infos.map((info)=> {
-        getInfos(info, pathFile);
-      })
-      return objInfo;
-    }
+  return new Promise ((resolve, reject)=>{
+    fs.readFile(pathFile, 'utf-8', (error, data)=>{
+      if(error){
+        reject(error); // o que era retorno, mudou
+      } else {
+        const infos = data.match(regEx); // array com todos os links
+        const objInfo = infos.map((info)=> {
+          getInfos(info, pathFile);
+        })
+        resolve(objInfo);
+      }
+    });
   });
 }
 
-// }
-
-// mdLinks('./files-test/fileMarkdown.md');
+  //   const fs = require('fs');
+  //   const regEx = /\[[^\]]+\]\(([^)]+)\)/gm; // gm para pegar varias linhas
+  // fs.readFile(pathFile, 'utf-8', (error, data)=>{
+  //   if(error){
+  //     return 'error';
+  //   } else {
+  //     const infos = data.match(regEx); // array com todos os links
+  //     const objInfo = infos.map((info)=> {
+  //       getInfos(info, pathFile);
+  //     })
+  //     return objInfo;
+  //   }
+  // });
